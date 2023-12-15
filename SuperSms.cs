f302cd2a3dc4ca12f3f52260b6d66c1d035ff3ab
havokzero <46027877+havokzero@users.noreply.github.com>
2023-12-14 22:15:25 -0700

    Update SuperSms.cs

--- SuperSms.cs
+++ SuperSms.cs
@@ -1,6 +1,7 @@
 using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
+using Mainboi;
 using Newtonsoft.Json;
 using Smguy;
 using System;
@@ -19,10 +20,11 @@ namespace Dboy
         private readonly Dictionary<string, List<string>> _allowedUsers;
         private readonly ulong _guildId;
         private readonly DiscordHandler _discordHandler;
+        private readonly PKeys _keys; // Add this field to store the configuration
 
         public SuperSmsHandler(DiscordSocketClient discordClient, SmsHandler smsHandler,
-            Dictionary<string, ulong> phoneNumberToUserId, Dictionary<string, List<string>> allowedUsers,
-            ulong guildId, DiscordHandler discordHandler)
+                       Dictionary<string, ulong> phoneNumberToUserId, Dictionary<string, List<string>> allowedUsers,
+                       ulong guildId, DiscordHandler discordHandler, PKeys keys)
         {
             _discordClient = discordClient;
             _smsHandler = smsHandler;
@@ -30,9 +32,17 @@ namespace Dboy
             _allowedUsers = allowedUsers;
             _guildId = guildId;
             _discordHandler = discordHandler;
+            _keys = keys;
+
 
             _discordClient.SlashCommandExecuted += OnSlashCommandExecutedAsync;
-            RegisterSlashCommands();
+
+            discordClient.Ready += async () =>
+            {
+                ulong searchResultsChannelId = ulong.Parse(_keys.discord.searchResultsChannelId);
+
+                RegisterSlashCommands();
+            }; // This should register / commands
         }
 
         private void RegisterSlashCommands()
@@ -45,7 +55,7 @@ namespace Dboy
                     new SlashCommandBuilder()
                         .WithName("help")
                         .WithDescription("Shows help information"),
-
+                                        
                     new SlashCommandBuilder()
                         .WithName("sms")
                         .WithDescription("Sends an SMS message")
@@ -57,7 +67,12 @@ namespace Dboy
                         .WithDescription("Sends multiple SMS messages")
                         .AddOption("phonenumber", ApplicationCommandOptionType.String, "Phone number to send the SMS", isRequired: true)
                         .AddOption("message", ApplicationCommandOptionType.String, "The message to send", isRequired: true)
-                        .AddOption("count", ApplicationCommandOptionType.Integer, "Number of times to send the message", isRequired: true)
+                        .AddOption("count", ApplicationCommandOptionType.Integer, "Number of times to send the message", isRequired: true),
+
+                    new SlashCommandBuilder() // Add a new command for /cherlock
+                         .WithName("cherlock")
+                         .WithDescription("Perform username search with Cherlock")
+                        .AddOption("username", ApplicationCommandOptionType.String, "Username to search for", isRequired: true)
                 };
 
                 foreach (var command in commands)
@@ -89,26 +104,28 @@ namespace Dboy
                         var countSpam = Convert.ToInt32(command.Data.Options.First(o => o.Name == "count").Value);
                         await ProcessSmsSpamCommandAsync(command, phoneNumberSpam, messageSpam, countSpam);
                         break;
-                    default:
-                        await command.RespondAsync("Invalid command.");
+                    case "cherlock":
+                        await command.DeferAsync(ephemeral: true); // Acknowledge the command
+                        var username = command.Data.Options.First(o => o.Name == "username").Value.ToString();
+                        await SearchUsernamesAsync(command, username); // Execute the search
                         break;
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error in OnSlashCommandExecutedAsync: {ex.Message}");
-                await command.RespondAsync("An error occurred while processing your command.");
+                await command.FollowupAsync("An error occurred while processing your command.");
             }
         }
 
         private async Task ProcessHelpCommandAsync(SocketSlashCommand command)
         {
             var embed = new EmbedBuilder()
-                .WithTitle("Help Information")
-                .WithDescription("Here are the available commands:\n" +
-                                 "- `/help`: Shows help information.\n" +
+                .WithTitle("SE Bot Help Information")
+                .WithDescription("Choose your vector in the realm of social engineering! Here are the available commands:\n" +
                                  "- `/sms <phonenumber> <message>`: Send an SMS to the specified phone number. Format the phone number as 1NXXNXXXXXX.\n" +
-                                 "- `/sms-spam <phonenumber> <message> <count>`: Send multiple SMS messages.\n")
+                                 "- `/sms-spam <phonenumber> <message> <count>`: Send multiple SMS messages.\n" +
+                                 "- `/cherlock <username>`: Search for usernames associated with the provided username on various platforms.\n")
                 .WithColor(Color.Red)
                 .WithCurrentTimestamp()
                 .Build();
@@ -219,6 +236,64 @@ namespace Dboy
             catch (Exception ex)
             {
                 await command.FollowupAsync($"Failed to send SMS spam: {ex.Message}");
+            }
+        }
+
+        public async Task SearchUsernamesAsync(SocketSlashCommand command, string input)
+        {
+            try
+            {
+                var usernames = input.Split(new[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+                var cherlock = new Cherlock(_keys);
+
+                foreach (var username in usernames)
+                {
+                    Console.WriteLine($"Searching for username: {username}");
+                    var results = await cherlock.SearchForUsernamesForDiscord(username);
+
+                    if (results.Length > 2000)
+                    {
+                        // Save lengthy results to a temporary file
+                        var tempFilePath = Path.Combine(Path.GetTempPath(), $"{username}.txt");
+                        await File.WriteAllTextAsync(tempFilePath, results);
+
+                        // Create an embed for the file and include the count of usernames found
+                        var embedBuilder = new EmbedBuilder();
+                        embedBuilder.AddField("Username Search Results", $"Results for '{username}' are too lengthy to display here. Please see the attached file.")
+                                     .AddField("Total Results", $"{cherlock.UsernameCount}");
+
+                        // Send the file with the embed
+                        await command.Channel.SendFileAsync(tempFilePath, embeds: new Embed[] { embedBuilder.Build() });
+
+                        // Clean up the temporary file
+                        File.Delete(tempFilePath);
+                    }
+                    else
+                    {
+                        // Create an embed for shorter results, splitting into multiple fields if necessary
+                        var embedBuilder = new EmbedBuilder();
+                        embedBuilder.AddField("Username Search Results", $"Results for '{username}':");
+
+                        const int maxFieldLength = 1024;
+                        while (results.Length > 0)
+                        {
+                            string fieldContent = results.Length <= maxFieldLength ? results : results.Substring(0, maxFieldLength);
+                            embedBuilder.AddField("\u200B", fieldContent); // Using a zero-width space as a field title
+                            results = results.Length <= maxFieldLength ? string.Empty : results.Substring(maxFieldLength);
+                        }
+
+                        // Send the embed with the results
+                        await command.Channel.SendMessageAsync(embed: embedBuilder.Build());
+                    }
+
+                    // Optionally, add a delay between processing each username
+                    await Task.Delay(1000); // 1 second delay
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error searching usernames: {ex.Message}");
+                await command.Channel.SendMessageAsync("An error occurred while searching for usernames.");
             }
         }
     }
